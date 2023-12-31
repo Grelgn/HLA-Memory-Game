091c043c06610f0b8b58e9a2f8549fc29953c7d3
Grelgn <grelgn@hotmail.com>
2024-01-01 02:05:34 +0300

    Fix header and title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
+import imgLogo from "/dist/assets/HLA_logo.png"
+
 function Header() {
     return (
         <header>
-            <img src="./src/assets/HLA_logo.png" alt="" height={75} />
+            <img src={imgLogo} height={75} />
             <h1>MEMORY GAME</h1>
         </header>
     );
