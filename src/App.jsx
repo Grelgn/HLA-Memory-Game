1960de8ed249c44f5609ea3fcaa5c033d2bae58b
Grelgn <grelgn@hotmail.com>
2023-12-31 17:01:56 +0300

    Add blue filter to card images

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Main from "./components/Game";
+import Game from "./components/Game";
 
 function App() {
 	return (
 		<>
 			<Header />
-			<Main />
+			<Game />
 			<Footer />
 		</>
 	);
